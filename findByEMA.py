3868870a7d2f9d3a57e6dcb84e0cfbefa9f5c3c0
julianchatto <juc226@lehigh.edu>
2024-07-20 19:53:01 -0400

    added a function to scrape for all s&p 500 tickers; can now determine if stock is bearish/bullish based off of 21 and 55 day EMA; writing results to files

--- findByEMA.py
+++ findByEMA.py
@@ -1,8 +1,9 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
-
-tickers = ["AAPL", "MSFT", "GOOGL", "AMZN", "META"]  
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
 
 ema_params = [
     ('21-day EMA', 'orange', 21),
@@ -11,16 +12,18 @@ ema_params = [
     # ('100-day EMA', 'red', 100)
 ]
 
+# calculate Exponential Moving Average
 def calculate_ema(data, window):
     return data['Close'].ewm(span=window, adjust=True).mean()
 
+# plot closing price with EMAs
 def plotData(data, ticker):
     fig = plt.figure(figsize=(12,6))
     ax1 = fig.add_subplot(111, ylabel='Price in $')
 
     data['Close'].plot(ax=ax1, color='b', lw=2.)
-    data['21-day EMA'].plot(ax=ax1, color='r', lw=2.)
-    data['55-day EMA'].plot(ax=ax1, color='g', lw=2.)
+    data['21-day EMA'].plot(ax=ax1, color='green', lw=2.)
+    data['55-day EMA'].plot(ax=ax1, color='red', lw=2.)
 
     ax1.plot(data.loc[data.crossover == 1.0].index, 
             data.Close[data.crossover == 1.0],
@@ -32,16 +35,53 @@ def plotData(data, ticker):
     plt.title(f'{ticker} Exponential Moving Average Crossover')
     plt.show()
 
-for ticker in tickers:
-    stock = yf.Ticker(ticker)
-    data = stock.history(period="1y")
-    
-    for ema_label, _, window in ema_params:
-        data[ema_label] = calculate_ema(data, window)
+# Scrape S&P 500 tickers
+def get_sp500():
+    url = 'https://www.slickcharts.com/sp500'
+    request = requests.get(url,headers={'User-Agent': 'Mozilla/5.0'})
+    soup = bs(request.text, "lxml")
+    stats = soup.find('table',class_='table table-hover table-borderless table-sm')
+    df = pd.read_html(str(stats))[0]
+    df['% Chg'] = df['% Chg'].str.strip('()-%')
+    df['% Chg'] = pd.to_numeric(df['% Chg'])
+    df['Chg'] = pd.to_numeric(df['Chg'])
+    return df['Symbol'].tolist()
 
-    data['bullish'] = 0.0
-    data['bullish'] = np.where(data['21-day EMA'] > data['55-day EMA'], 1.0, 0.0)
-    data['crossover'] = data['bullish'].diff()
-    
+def main():
+    tickers = get_sp500()
+    bullish = []
+    bearish = []
+    for ticker in tickers:
+        try: 
+            stock = yf.Ticker(ticker)
+            data = stock.history(period="1y")
+            
+            for ema_label, _, window in ema_params:
+                data[ema_label] = calculate_ema(data, window)
 
-    plotData(data, ticker)
+            data['bullish'] = 0.0
+            data['bullish'] = np.where(data['21-day EMA'] > data['55-day EMA'], 1.0, 0.0)
+            data['crossover'] = data['bullish'].diff()
+            
+            if (data['21-day EMA'].iloc[-1] > data['55-day EMA'].iloc[-1]):
+                bullish.append(ticker)
+            else:
+                bearish.append(ticker)
+        except: 
+            print("Error: ", ticker)
+            continue
+        
+
+        # plotData(data, ticker)
+        
+    with open("bullish.txt", 'w') as f:
+        for stock in bullish:
+            f.write(f"{stock}\n")
+        f.close()
+            
+    with open("bearish.txt", 'w') as f:
+        for stock in bearish:
+            f.write(f"{stock}\n")
+        f.close()
+    
+main()
